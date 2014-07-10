2d945502bbc00008c1c7c2805570bab6cd36b56e
dankernel <google@dankook.ac.kr>
2014-07-10 11:37:56 +0900

    auto-20140710-113756

---
+++ Exercise/204/main.c
@@ -0,0 +1,25 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2014년 07월 10일 11시 37분 50초
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Jun-Hyung Park, google@dankook.ac.kr
+ *   Organization:  Dankook Univ.
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+
+  return 0;
+}
+
