2b46d9b45b871ddf062889d0344f065832bd1c05
dankernel <google@dankook.ac.kr>
2014-07-11 01:20:01 +0900

    auto-20140711-012001

--- Exercise/203/main.c
+++ Exercise/203/main.c
@@ -17,7 +17,7 @@
  */
 #include <stdlib.h>
 #include <stdio.h>
-#include <linits.h>
+#include <limits.h>
 
 #define MAX 1024
 
@@ -25,32 +25,38 @@ int htoi(char *get){
 
   int ret = 0;
   int i = 0;
-  int tmp_i
+  int tmp_i;
   char tmp_c = '\0';
 
   if (!get)
     return -1;
 
   tmp_c = *(get + i);
-  while (tmp != EOF) {
+  printf("%c \n", tmp_c);
+  while (tmp_c != '\n') {
+
     if ('0' <= tmp_c && tmp_c <= '9')
       tmp_i = tmp_c - '0';
     else if ('A' <= tmp_c && tmp_c <= 'Z')
-      tmp_i = tmp_c - 'A';
+      tmp_i = tmp_c - 'A' + 10;
     else if ('a' <= tmp_c && tmp_c <= 'z')
-      tmp_i = tmp_c - 'a';
+      tmp_i = tmp_c - 'a' + 10;
     else
       return -2;
 
-    ret = tmp_i;
-    ret <<= 2;
+    ret <<=1;
+    ret += tmp_i;
+    printf("ret1 : %d \n", ret);
 
     i++;
     tmp_c = *(get + i);
   }
 
+  
   if (INT_MAX <= ret)
-    return -2;
+    return -3;
+
+  ret >>= 1;
 
   return ret;
 }
@@ -58,9 +64,11 @@ int htoi(char *get){
 int main(int argc, char* argv[]) {
 
   char str[MAX];
+  int ret = 0;
   fgets(str, MAX, stdin);
 
-  htoi();
+  ret = htoi(str);
+  printf("%d \n", ret);
 
 
 
