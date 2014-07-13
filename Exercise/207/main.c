444c3d92412e56b53d3291caeeeeb9c22dd09541
dankernel <google@dankook.ac.kr>
2014-07-13 17:40:01 +0900

    auto-20140713-174001

--- Exercise/207/main.c
+++ Exercise/207/main.c
@@ -25,14 +25,24 @@ int invert(int num, int bitp, int bitn)
   //get bit ..//
   tmp_bit = ~num & (((1<<bitn) - 1) << bitp-bitn);
   printf("bit : %d \n", tmp_bit);
+  print_bin(tmp_bit);
 
   //set 0 ..//
   num = num & ~(((1<<bitn) - 1) << bitp-bitn);
   printf("num : %d \n", num);
+  print_bin(num);
 
   num = num & tmp_bit;
   printf("num : %d \n", num);
 
+}
+
+void print_bin(unsigned int num)
+{
+  int i = 32;
+  while (0 <= i)
+    printf("%d", (num>>i--) & 1);
+  printf("\n\n");
 }
 
 int main(int argc, char* argv[]) {
