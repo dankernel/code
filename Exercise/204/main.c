072ae228feb91d2f964670b735d827c9dc7c5c0a
dankernel <google@dankook.ac.kr>
2014-07-13 15:50:01 +0900

    auto-20140713-155001

--- Exercise/204/main.c
+++ Exercise/204/main.c
@@ -25,28 +25,24 @@ char* squeeze(char *str1, char *str2)
   int len2 = strlen(str2);
   int i = 0, j = 0;
   int tmp = 0;
-  char ret[100] = {'\0', };
+  char *ret;
   int ind = 0;
+
+  ret = malloc(100);
+  memset(ret, '\0', 100);
 
   if (!str1 || !str2)
     return NULL;
 
   while (*(str2 + j) != '\0') {
 
-    printf("A : %s \n", str2);
-    printf("B : %s \n", str1);
-    printf("i : %d \n", i);
-    printf("j : %d \n", j);
     while (*(str1 + i) == *(str2 + j)) {
       i++;
       j++;
-      printf("loop\n");
     }
-    
+
     if (i == len1) {
-      printf("ok \n");
     } else {
-      printf("no \n");
       ret[ind++] = *(str2 + j);
       printf("__ret : %s \n", ret);
       j++;
@@ -57,14 +53,14 @@ char* squeeze(char *str1, char *str2)
   }
   printf("ret : %s \n", ret);
 
-  return str2;
+  return ret;
 }
 
 int main(int argc, char* argv[]) {
 
   char *ret = NULL;
 
-  ret = squeeze("ab", "ab1ab2ab3a4");
+  ret = squeeze("ab", "ab12ab34ab56");
   printf("%s \n", ret);
 
   return 0;
--- Exercise/205/main.c
+++ Exercise/205/main.c
@@ -17,8 +17,35 @@
  */
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+
+char *any(char *str1, char *str2)
+{
+  int len1 = strlen(str1);
+  int len2 = strlen(str2);
+  int i = 0, j = 0;
+
+  while (j < len2) {
+   
+    while (*(str1 + i) == *(str2 + j))
+      i++, j++;
+
+    if (i == len1)
+      return (str2 + j - len1);
+    else
+      i = 0, j++;
+  }
+
+  return NULL;
+}
 
 int main(int argc, char* argv[]) {
+
+  char *ret = NULL;
+
+  ret = any("ab", "123ab45");
+  printf("%s \n", ret);
+
 
   return 0;
 }
