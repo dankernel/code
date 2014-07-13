5002e2dabd870d3794673e2b6271bf2bef4a4236
dankernel <google@dankook.ac.kr>
2014-07-13 14:30:01 +0900

    auto-20140713-143001

--- Exercise/204/main.c
+++ Exercise/204/main.c
@@ -25,18 +25,22 @@ char* squeeze(char *str1, char *str2)
   int len2 = strlen(str2);
   int i = 0, j = 0;
   int ret_i = 0;
+  char ret[100];
 
   if (!str1 || !str2)
     return NULL;
 
-  while (j < len2) {
-    while (*(str1 + i) == *(str2 + j))
-      i = ++j;
+  while (*(str2 + j) != '\0') {
+    while (*(str1 + i) == *(str2 + j)) {
+      i++;
+      j++;
+      printf("loop\n");
+    }
 
-    printf("1\n");
-    memcpy(&str2, (&str2 + j), len2 - j - 1);
-    printf("2\n");
-    j++;
+    printf("%d %d \n", i, j);
+    memcpy(&str2, (&str2 + j), len2 - j - 4);
+
+    j = 0;
     i = 0;
   }
 
@@ -47,7 +51,7 @@ int main(int argc, char* argv[]) {
 
   char *ret = NULL;
 
-  ret = squeeze("as", "ab1ab2ab3");
+  ret = squeeze("ab", "ab1ab2ab3");
   printf("%s \n", ret);
 
   return 0;
