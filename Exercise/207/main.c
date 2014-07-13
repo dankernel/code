e439205e3ab8e92ff4da0a7feb11bcd1e692596e
dankernel <google@dankook.ac.kr>
2014-07-13 17:00:01 +0900

    auto-20140713-170001

--- Exercise/207/main.c
+++ Exercise/207/main.c
@@ -6,7 +6,7 @@
  *    Description:  
  *
  *        Version:  1.0
- *        Created:  2014년 07월 13일 15시 52분 06초
+ *        Created:  2014년 07월 13일 16시 57분 29초
  *       Revision:  none
  *       Compiler:  gcc
  *
@@ -18,31 +18,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int setbit(int x, int p, int n, int y)
-{
-  int ret = 0;
-  int bit = 0;
-
-  //get  bit..//
-  bit = y & ( (1<<n-p+1) - 1 );
-  printf("%d \n", bit);
-
-  //set 0bit ..//
-  x &= ~( ((1<<n-p+1) - 1) << (n-p+1));
-  printf("%d \n", x);
-
-  ///or ..//
-  x |= (bit << (n-p+1));
-  printf("%d \n", x);
-
-  return ret;
-
-}
-
 int main(int argc, char* argv[]) {
 
-  setbit(33, 1, 3, 7);
+
+
+
+
 
   return 0;
 }
-
