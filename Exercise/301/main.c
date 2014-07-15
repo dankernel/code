d8be7f7ab04d5dedeeeba0fe919e14876b039ca3
dankernel <google@dankook.ac.kr>
2014-07-15 11:30:01 +0900

    auto-20140715-113001

---
+++ Exercise/301/main.c
@@ -0,0 +1,25 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2014년 07월 15일 11시 25분 17초
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         author:   (), 
+ *   organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+
+  return 0;
+}
+
