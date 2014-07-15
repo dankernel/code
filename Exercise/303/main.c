d074c82d3eab7d04bd6343cfd801e814402b12ee
dankernel <google@dankook.ac.kr>
2014-07-15 12:10:01 +0900

    auto-20140715-121001

--- Exercise/303/main.c
+++ Exercise/303/main.c
@@ -17,14 +17,44 @@
  */
 #include <stdlib.h>
 #include <stdio.h>
+#define MAX 100
+
+char *_expand(char start, char end)
+{
+  int i, j = 0;
+  char *ret = NULL;
+  ret = malloc(sizeof(char) * MAX);
+
+  for (i=start; i<=end; i++)
+    *(ret + j++) = i;
+  
+  return ret;
+}
 
 char *expand(char *str1, char *str2)
 {
   int i = 0, j = 0;
   char *ret = NULL;
+  char *tmp;
+  char start, end;
 
   if (!str1 || !str2)
     return NULL;
+
+  while (*(str1 + i)){
+  
+    tmp = (str1 + i);
+    if (*tmp == '-'){
+      start = *(tmp - 1);
+      end = *(tmp + 1);
+    }
+
+    _expand(start, end);
+
+    
+    //next..//
+    i++;
+  }
   
 
 
@@ -33,6 +63,13 @@ char *expand(char *str1, char *str2)
 }
 
 int main(int argc, char* argv[]) {
+
+  char str1[MAX] = "a-z";
+  char str2[MAX] = {'\0', };
+  char *ret = NULL;
+
+  ret = expand(str1, str2);
+  printf("%s\n", ret);
 
   return 0;
 }
