07a21fd3a564c4861b38c9ee8e94f06bd302eddf
dankernel <google@dankook.ac.kr>
2014-07-17 17:10:01 +0900

    auto-20140717-171001

--- Exercise/305/main.c
+++ Exercise/305/main.c
@@ -21,10 +21,7 @@
 
 char *itob(char *ret, int num, int b)
 {
-  int i = 0, j = 0;
-  int tmp;
-  int unit = 0;
-  int len = 0;
+  int i = 0, tmp = 0, unit = 0, len = 0;
   char *ret_tmp = NULL;
 
   if (!ret || b <= 0)
@@ -36,7 +33,6 @@ char *itob(char *ret, int num, int b)
   //get unit..//
   while (b >>= 1)
     unit++;
-  printf("unit : %d \n", unit);
 
   //getindex..//
   len = 32 / unit;
@@ -45,7 +41,6 @@ char *itob(char *ret, int num, int b)
 
   while (0 <= i) {
     tmp = (num >> unit * i) & ((1<<unit) - 1);
-    printf("i : %d / tmp : %d \n", i, tmp);
 
     if (tmp <= 9)
       tmp += '0';
@@ -53,19 +48,19 @@ char *itob(char *ret, int num, int b)
       tmp = 'A' + (tmp - 10);
 
     *(ret_tmp + len - i - 1) = tmp;
-
-    printf("loop\n");
     i--;
   }
 
   printf("ret : %s \n", ret_tmp);
+  ret = ret_tmp;
+
   return ret_tmp;
 }
 
 int main(int argc, char* argv[]) {
 
   char ret[32];
-  itob(ret, 255, 16);
+  itob(ret, 9, 2);
 
   return 0;
 }
