29431ef8e8cd2164f394c41ce9eb494e10a69dfa
dankernel <google@dankook.ac.kr>
2014-07-25 06:40:03 +0900

    auto-20140725-064003

---
+++ tmp/test/main.c
@@ -0,0 +1,30 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2014년 07월 25일 06시 34분 33초
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         author:  Jun-Hyung Park (), google@dankook.ac.kr
+ *   organization:  Dankook Univ.
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char* argv[])
+{
+  int i;
+  while (1)
+    i++;
+
+  return 0;
+}
+
