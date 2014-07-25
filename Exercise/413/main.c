ca3dfe9ccfe8cb8e90f5b926e081a804b82f871b
dankernel <google@dankook.ac.kr>
2014-07-26 01:40:02 +0900

    auto-20140726-014002

--- Exercise/413/main.c
+++ Exercise/413/main.c
@@ -20,16 +20,37 @@
 #include <string.h>
 
 /*  swap: interchange v[i] and v[j] */
-void swap(int v[], int i, int j)
+inline void swap(int v[], int i, int j)
 {
   int temp;
   temp = v[i];
   v[i] = v[j];
   v[j] = temp;
+}
+
+/*  reverse: reverse string s in place */
+void reverse(char s[])
+{
+  int c, i, j;
+
+  if (i = 0, j = strlen(s)-1; i < j; i++, j--) {
+
+    //swap..///
+    c = s[i];
+    s[i] = s[j];
+    s[j] = c;
+  }
+
+  reverse(s+1);
 } 
 
 int main(int argc, char* argv[])
 {
+  char str[100] = "I'm_dkdk509";
+
+  printf("%s\n", str);
+  reverse(str);
+  printf("%s\n", str);
 
   return 0;
 }
