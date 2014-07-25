5e867ccee4234635ee2967bda60e40854287df0e
dankernel <google@dankook.ac.kr>
2014-07-25 20:10:01 +0900

    auto-20140725-201001

--- project/main.java
+++ project/main.java
@@ -1,12 +1,83 @@
 
+import java.*;
 
 public class main
 {
-  public static int main(String[] args){
-    int ret = 0;
+  
+  public static void main(String[] args){
 
-    System.out.println("asad");
+    System.out.println("=== Start ===");
 
-    return ret;
+    val test[10];
+
   }
+
+}
+
+/*
+ * val of all type.
+ */
+class val
+{
+  String name;
+  //enum type;
+}
+
+class math
+{
+  math val1;
+  math val2;
+
+  //enum op;      
+}
+
+/* +, - , *, /, %, */
+/* +=, -=, *=, /=, %=, */
+/* ==, !=, <=, >=, <, >, */
+/* ~, ^, |, & */
+
+class call
+{
+  int argc;
+  String argv[];
+  //or val argv[];
+
+  String func;  //function name..//
+}
+
+class code
+{
+  Object node;
+}
+
+/*
+ * used branch.
+ */
+class code_s
+{
+  code list[]; 
+}
+
+//math result => code_s
+class mapp_table
+{
+  int result[];
+  int index[];
+}
+
+class branch
+{
+  Object math;
+  Object code_s[];
+
+  mapp_table table; //math result and code array.//
+}
+
+class set_func
+{
+  int argc;
+  String argv[];
+
+  String func;  //function name..//
+  Object code_s[];
 }
