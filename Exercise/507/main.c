572fdd716dcff9b5e67564c13cd14c5ed82f3689
dankernel <google@dankook.ac.kr>
2014-07-31 21:00:01 +0900

    auto-20140731-210001

--- Exercise/507/main.c
+++ Exercise/507/main.c
@@ -25,6 +25,19 @@ char *lineptr[MAXLINES]; /*  pointers to text lines */
 int _getline(char *, int);
 char *alloc(int);
 
+#define ALLOCSIZE 10000 /*  size of available space */
+static char allocbuf[ALLOCSIZE]; /*  storage for alloc */
+static char *allocp = allocbuf; /*  next free position */
+
+char *alloc(int n) /*  return pointer to n characters */
+{
+  if (allocbuf + ALLOCSIZE - allocp >= n) { /*  it fits */
+    allocp += n;
+    return allocp - n; /*  old p */
+  } else /*  not enough room */
+    return 0;
+}
+
 /* readlines: read input lines */
 int readlines(char *lineptr[], int maxlines)
 {
@@ -32,7 +45,7 @@ int readlines(char *lineptr[], int maxlines)
   char *p, line[MAXLEN];
   nlines = 0;
   while ((len = _getline(line, MAXLEN)) > 0)
-    if (nlines >= maxlines || p = alloc(len) == NULL)
+    if (nlines >= maxlines || (p = alloc(len)) == NULL)
       return -1;
     else {
       line[len-1] = '\0'; /*  delete newline */
