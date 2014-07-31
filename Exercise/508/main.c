94fb25c5355a16a33f9ceb3144024c2e259217c6
dankernel <google@dankook.ac.kr>
2014-07-31 20:50:01 +0900

    auto-20140731-205001

---
+++ Exercise/508/main.c
@@ -0,0 +1,58 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2014년 07월 31일 20시 45분 34초
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         author:  Jun-Hyung Park (), google@dankook.ac.kr
+ *   organization:  Dankook Univ.
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+static char daytab[2][13] = {
+  {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
+  {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
+};
+
+/*  day_of_year: set day of year from month & day */
+int day_of_year(int year, int month, int day)
+{
+  int i, leap;
+  leap = year%4 == 0 && year%100 != 0 || year%400 == 0;
+  for (i = 1; i < month; i++)
+    day += daytab[leap][i];
+
+  return day;
+} 
+
+/*  month_day: set month, day from day of year */
+void month_day(int year, int yearday, int *pmonth, int *pday)
+{
+  int i, leap;
+  leap = year%4 == 0 && year%100 != 0 || year%400 == 0;
+  for (i = 1; yearday > daytab[leap][i]; i++)
+    yearday -= daytab[leap][i];
+  *pmonth = i;
+  *pday = yearday;
+} 
+
+int main(int argc, char* argv[])
+{
+  int ret = 0;
+
+  ret = day_of_year(2014, 2, 4);
+  printf("%d\n", ret);
+
+  return 0;
+}
+
