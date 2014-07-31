893dffe409a8e0a6355860ea6de7fcd80d8740a3
dankernel <google@dankook.ac.kr>
2014-07-31 09:00:01 +0900

    auto-20140731-090001

--- Exercise/502/main.c
+++ Exercise/502/main.c
@@ -20,21 +20,39 @@
 #include <string.h>
 #include <ctype.h>
 
+#define BUFSIZE 100
+char buf[BUFSIZE]; /*  buffer for ungetch */
+int bufp = 0; /*  next free position in buf */ 
+
+int getch(void) /*  get a (possibly pushed-back) character */
+{
+  return (bufp > 0) ? buf[--bufp] : getchar();
+}
+void ungetch(int c) /*  push character back on input */
+{
+  printf("%d \n", c);
+  if (bufp >= BUFSIZE)
+    printf("ungetch: too many characters\n");
+  else
+    buf[bufp++] = c;
+}
+
 /*  getint: get next integer from input into *pn */
-int getint(int *pn)
+int getint(char *pn)
 {
   int c, sign;
   while (isspace(c = getch())); /*  skip white space */
   
-  if (!isdigit(c) && c != EOF && c != '+' && c != '-') {
+  if (!isdigit(c) && c != EOF && c != '+' && c != '-' && c != '.') {
     ungetch(c); /*  it is not a number */
     return 0;
   }
+
   sign = (c == '-') ? -1 : 1;
   if (c == '+' || c == '-')
     c = getch();
 
-  for (*pn = 0; isdigit(c), c = getch();)
+  for (*pn = 0; isdigit(c); c = getch())
     *pn = 10 * *pn + (c - '0');
 
   *pn *= sign;
@@ -46,6 +64,12 @@ int getint(int *pn)
 
 int main(int argc, char* argv[])
 {
+
+  int ret = 0;
+  char p[10];
+
+  ret = getint(p);
+  printf("%d\n", *p);
 
 
   return 0;
