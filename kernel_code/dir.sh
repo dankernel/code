6353bad13123ffc81839d001c4867bc43da6d4af
dankernel <google@dankook.ac.kr>
2014-08-05 05:00:01 +0900

    auto-20140805-050001

--- kernel_code/dir.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-for loopdirectory in `ls -d ./*`; do
-  echo $loopdirectory;
-  for datadirectory in `ls $loopdirectory/*`; do
-    echo $loopdirectory/$datadirectory;
-    ./main $datadirectory
-  done;
-done;
