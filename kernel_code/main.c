96e24e4081fcc49e0e5a641019edc1110e3aed07
dankernel <google@dankook.ac.kr>
2014-08-05 03:50:05 +0900

    auto-20140805-035005

---
+++ kernel_code/main.c
@@ -0,0 +1,30 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2014년 08월 05일 03시 41분 16초
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         author:  Jun-Hyung Park (), google@dankook.ac.kr
+ *   organization:  Dankook Univ.
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char* argv[])
+{
+  int fd = 0;
+
+  fd = open_file();
+
+  return 0;
+}
+
