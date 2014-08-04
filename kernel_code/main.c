0cce375de9229167f691220ce3f6068d886ee842
dankernel <google@dankook.ac.kr>
2014-08-05 04:00:01 +0900

    auto-20140805-040001

--- kernel_code/main.c
+++ kernel_code/main.c
@@ -18,12 +18,58 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <fcntl.h>
+
+#define ERR_ARG_NULL 0x00000001
+
+int open_file(char *path)
+{
+  int fd = 0;
+
+  if(!path)
+    return -ERR_ARG_NULL;
+
+  if ((fd = open(path, O_RDONLY)) < 0)
+    return -1;
+  else
+    return fd;
+}
+
+int read_file(char *path, int fd)
+{
+  int size = 0;
+  int ret = 0;
+  struct stat file_info;
+  char *buf = NULL;
+
+  /* get file size */
+  stat(path, &file_info);
+  size = file_info.st_size;
+  printf("size : %d \n", size);
+
+  /* init buf */
+  buf = malloc(sizeof(char) * size);
+
+  if ((
+
+
+
+
+
+
+
+  return 0;
+
+}
+
 
 int main(int argc, char* argv[])
 {
   int fd = 0;
 
-  fd = open_file();
+  fd = open_file("./file.c");
+  
+  read_file("./file.c", fd);
 
   return 0;
 }
