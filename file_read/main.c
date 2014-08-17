7ba086157a5bea851b430f855eb969ae586d4f63
dankernel <google@dankook.ac.kr>
2014-08-18 02:50:12 +0900

    auto-20140818-025012

---
+++ file_read/main.c
@@ -0,0 +1,44 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  file read header
+ *
+ *        Version:  1.0
+ *        Created:  2014년 08월 18일 02시 46분 01초
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         author:  Jun-Hyung Park (), google@dankook.ac.kr
+ *   organization:  Dankook Univ.
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+
+int open_file(char *path)
+{
+  int ret = -1;
+
+ret:
+  return 0;
+}
+
+int main(int argc, char* argv[])
+{
+  int fd = -1;
+
+  open_file();
+  
+
+
+
+
+
+  return 0;
+}
+
