db7c426ad545a07b1b3522677dd3121ede76a545
dankernel <google@dankook.ac.kr>
2014-08-18 10:10:01 +0900

    auto-20140818-101001

--- Exercise/514/main.c
+++ Exercise/514/main.c
@@ -31,7 +31,7 @@ void qsort(void *lineptr[], int left, int right, int (*comp)(void *, void *));
 int numcmp(char *, char *);
 
 /*  sort input lines */
-int main(int argc, char *argv[])
+main(int argc, char *argv[])
 {
   int nlines; /*  number of input lines read */
   int numeric = 0; /*  1 if numeric sort */
@@ -69,9 +69,24 @@ void qsort(void *v[], int left, int right, int (*comp)(void *, void *))
   qsort(v, last+1, right, comp);
 }
 
-int main(int argc, char* argv[])
+/*  numcmp: compare s1 and s2 numerically */
+int numcmp(char *s1, char *s2)
 {
-
-  return 0;
+  double v1, v2;
+  v1 = atof(s1);
+  v2 = atof(s2);
+  if (v1 < v2)
+    return -1;
+  else if (v1 > v2)
+    return 1;
+  else
+    return 0;
 }
 
+void swap(void *v[], int i, int j;)
+{
+  void *temp;
+  temp = v[i];
+  v[i] = v[j];
+  v[j] = temp;
+}
