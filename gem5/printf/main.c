e9417b1fe87ae2c570ee8e7747e5ce3683c00820
dankernel <google@dankook.ac.kr>
2014-08-23 07:00:01 +0900

    auto-20140823-070001

---
+++ gem5/printf/main.c
@@ -0,0 +1,26 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2014년 08월 23일 06시 54분 02초
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         author:  Jun-Hyung Park (), google@dankook.ac.kr
+ *   organization:  Dankook Univ.
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+  printf("DKDK Hihi\n");
+
+  return 0;
+}
+
