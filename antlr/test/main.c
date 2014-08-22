95efaec0a130482327705781a461e7b057de76f9
dankernel <google@dankook.ac.kr>
2014-08-22 20:30:03 +0900

    auto-20140822-203003

--- antlr/test/main.c
+++ antlr/test/main.c
@@ -17,15 +17,4 @@ int main()
   printf("result : %d \n", a);
 }
 
-(FUNCTION (KEYWORD int) (DECLARATOR (ID f1) ( (KEYWORD int) (ID num) )) { 
- (STATEMENT (KEYWORD return) (ID num) * (LITERAL 2) ;) 
-}) 
-
-(FUNCTION (KEYWORD int) (DECLARATOR (ID main) ( )) { 
- (DECLARATION (KEYWORD int) (INIT_DECLARATOR (DECLARATOR (ID a)) = (LITERAL 10)) ;) 
- (DECLARATION (KEYWORD int) (INIT_DECLARATOR (DECLARATOR * (ID p)) = (ID NULL)) ;) 
- (STATEMENT (ID p) = & (ID a) ;) 
- (STATEMENT (ID a) = (ID f1) ( * (ID p) ) ;) 
- (STATEMENT (ID printf) ( (LITERAL "result : %d \n") , (ID a) ) ;)
-})
 
