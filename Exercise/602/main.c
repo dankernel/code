e343ef82dffe50070c8d29c77d1c9cff71766fd6
dankernel <google@dankook.ac.kr>
2014-08-30 01:00:01 +0900

    auto-20140830-010001

--- Exercise/602/main.c
+++ Exercise/602/main.c
@@ -124,11 +124,11 @@ int main(int argc, char* argv[])
   struct tnode *root;
   char word[MAXWORD];
   root = NULL;
+
   while (getword(word, MAXWORD) != EOF)
     if (isalpha(word[0]))
       root = addtree(root, word);
   treeprint(root);
   return 0;
 }
-
 
