bf94af0d524ebc8664b585b976b1a0bbd790fc7b
dankernel <google@dankook.ac.kr>
2014-08-30 01:20:01 +0900

    auto-20140830-012001

--- Exercise/602/main.c
+++ Exercise/602/main.c
@@ -125,9 +125,10 @@ int main(int argc, char* argv[])
   char word[MAXWORD];
   root = NULL;
 
-  while (getword(word, MAXWORD) != EOF)
+  while (getword(word, MAXWORD) != EOF) {
     if (isalpha(word[0]))
       root = addtree(root, word);
+  }
   treeprint(root);
   return 0;
 }
