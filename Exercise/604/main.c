2d26b022931aca55f0b299605ab69ba354179b45
dankernel <google@dankook.ac.kr>
2014-09-04 06:00:01 +0900

    auto-20140904-060001

--- Exercise/604/main.c
+++ Exercise/604/main.c
@@ -120,15 +120,16 @@ struct tnode *addtree(struct tnode *p, char *w)
   int cond;
   if (p == NULL) { /*  a new word has arrived */
     p = talloc(); /*  make a new node */
-    p->word = _strndup(w, 6);
+    p->word = strdup(w);
     p->count = 1;
     p->left = p->right = NULL;
-  } else if ((cond = _strncmp(w, p->word, 6)) == 0)
+  } else if ((cond = strcmp(w, p->word)) == 0) {
     p->count++; /*  repeated word */
-  else if (cond < 0) /*  less than into left subtree */
+  } else if (cond < 0) { /*  less than into left subtree */
     p->left = addtree(p->left, w);
-  else /*  greater than into right subtree */
+  } else { /*  greater than into right subtree */
     p->right = addtree(p->right, w);
+  }
   return p;
 }
 
