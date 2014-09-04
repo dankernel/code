57c7044d02eb121b9f3bf7c3d7575dbf39681897
dankernel <google@dankook.ac.kr>
2014-09-05 01:50:01 +0900

    auto-20140905-015001

--- Exercise/604/main.c
+++ Exercise/604/main.c
@@ -164,8 +164,8 @@ void treeprint(struct tnode *p)
 }
 
 int node_cmp(const void *a , const void *b){
-  struct tnode* node1 = (struct tnode*)a;
-  struct tnode* node2 = (struct tnode*)b;
+  struct tnode *node1 = (struct tnode*)a;
+  struct tnode *node2 = (struct tnode*)b;
 
   printf("sort.. %d / %d \n", node1->count, node2->count);
 
@@ -191,7 +191,7 @@ int main(int argc, char *argv[])
   printf("index : %d \n", list_index);
 
   /* sort */
-  qsort(&tnode_list, list_index - 1, sizeof(tnode_list[0]), node_cmp);
+  qsort(tnode_list, list_index, sizeof(tnode_list[0]), node_cmp);
 
   printf("[OK] Sort\n");
 
