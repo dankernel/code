342fe453f8933c57d3af5c0b1bee556b327f4951
dankernel <google@dankook.ac.kr>
2014-09-09 10:40:02 +0900

    auto-20140909-104002

--- dk_chat/src/com/worklover/dk_chat/MainActivity.java
+++ dk_chat/src/com/worklover/dk_chat/MainActivity.java
@@ -1,14 +1,39 @@
 package com.worklover.dk_chat;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gcm.GCMRegistrar;
 
 public class MainActivity extends Activity {
+
+	private String html = "";
+	private Handler mHandler;
+	private Socket socket;
+	private String name;
+	private BufferedReader networkReader;
+	private BufferedWriter networkWriter;
+	private String ip = "xxx.xxx.xxx.xxx"; // IP
+	private int port = 9999; // PORT번호
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +62,33 @@ public class MainActivity extends Activity {
 		return super.onOptionsItemSelected(item);
 	}
 
+	public void sock(){
+		try {
+			setSocket(ip, port);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+		checkUpdate.start();
+		final EditText et = (EditText) findViewById(R.id.EditText01);
+		Button btn = (Button) findViewById(R.id.Button01);
+		final TextView tv = (TextView) findViewById(R.id.TextView01);
+
+		btn.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(DialogInterface arg0, int arg1) {
+				// TODO Auto-generated method stub
+				if (et.getText().toString() != null
+						|| !et.getText().toString().equals("")) {
+					PrintWriter out = new PrintWriter(networkWriter,true);
+					String return_msg = et.getText().toString();
+					out.println(return_msg);
+				}
+			}
+		});
+	}
+
 	public void registerGcm() {
 		Log.e("reg_id", "Start")
 		;
@@ -49,7 +101,42 @@ public class MainActivity extends Activity {
 		} else {
 			Log.e("reg_id", regId);
 		}
-		
+
+	}
+
+	private Thread checkUpdate = new Thread() {
+		public void run() {
+			try {
+				String line;
+				Log.w("ChattingStart", "Start Thread");
+				while (true) {
+					Log.w("Chatting is running", "chatting is running");
+					line = networkReader.readLine();
+					html = line;
+					mHandler.post(showUpdate);
+
+				}
+			} catch (Exception e) {
+			}
+		}
+	};
+
+	private Runnable showUpdate = new Runnable() {
+		public void run() {
+			Toast.makeText(MainActivity.this, "Coming word: " + html, Toast.LENGTH_SHORT).show();
+		}
+	};
+
+	public void setSocket(String ip, int port) throws IOException {
+		try {
+			socket = new Socket(ip, port);
+			networkWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+			networkReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		} catch (IOException e) {
+			System.out.println(e);
+			e.printStackTrace();
+		}
+
 	}
 
 }
