f0c5696042be6229e2666bd349930966e3b051c5
dankernel <google@dankook.ac.kr>
2014-09-12 19:50:01 +0900

    auto-20140912-195001

---
+++ list_lib/main.c
@@ -0,0 +1,33 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2014년 09월 12일 19시 47분 12초
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         author:  Jun-Hyung Park (), google@dankook.ac.kr
+ *   organization:  Dankook Univ.
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+
+// #include "dk_list.h"
+
+int main(int argc, char* argv[])
+{
+
+  printf("dkdk \n");
+
+
+  return 0;
+}
+
