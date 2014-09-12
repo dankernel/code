e3802739aed532d40a5e251d4fa9aad906794c99
dankernel <google@dankook.ac.kr>
2014-09-12 19:30:03 +0900

    auto-20140912-193003

---
+++ read_code/main.c
@@ -0,0 +1,55 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2014년 08월 18일 03시 08분 19초
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         author:  Jun-Hyung Park (), google@dankook.ac.kr
+ *   organization:  Dankook Univ.
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+
+//#include "code_analyze.h"
+#include "dkh/file_read.h"
+#include "dkh/print_msg.h"
+
+int main(int argc, char* argv[])
+{
+  int fd = -1;
+  int ret = 0;
+  char *buf = NULL;
+
+  /* alloc and init struct */
+  struct file_info *dk_file = NULL;
+  dk_file = (struct file_info*)malloc(sizeof(struct file_info));
+  init_file_struct(dk_file, "./test/core.c");
+
+  /* CORE */
+  buf = read_split(dk_file, '\n');
+  while (buf) {
+
+    // cheek //
+    printf("%s\n", buf);
+
+    /*
+     * SOME CODE ...
+     * ...
+     */
+
+    // next //
+    buf = read_split(dk_file, '\n');
+  }
+
+  return 0;
+}
