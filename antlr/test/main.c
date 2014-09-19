1d905fd654503162334940bfafa201909de087c4
dankernel <google@dankook.ac.kr>
2014-09-19 19:10:02 +0900

    auto-20140919-191002

--- antlr/test/main.c
+++ antlr/test/main.c
@@ -1,32 +1,12 @@
 
 #include <stdio.h>
 
-int f1(int num)
-{
-  return num * 2;
-}
-
 int main()
 {
-  int a = 10;
-  int *p = NULL;
+  int a;
 
-  p = &a;
-  a = f1(*p);
+  a = 10.4 + 20 * 30 - 10;
 
-  printf("result : %d \n", a);
+  return 0;
 }
-
-
-(FUNCTION (KEYWORD int) (DECLARATOR (ID f1) ( (KEYWORD int) (ID num) )) { 
- (STATEMENT (KEYWORD return) (ID num) * (LITERAL 2) ;) 
-}) 
-
-(FUNCTION (KEYWORD int) (DECLARATOR (ID main) ( )) { 
- (DECLARATION (KEYWORD int) (INIT_DECLARATOR (DECLARATOR (ID a)) = (LITERAL 10)) ;) 
- (DECLARATION (KEYWORD int) (INIT_DECLARATOR (DECLARATOR * (ID p)) = (ID NULL)) ;) 
- (STATEMENT (ID p) = & (ID a) ;) 
- (STATEMENT (ID a) = (ID f1) ( * (ID p) ) ;) 
- (STATEMENT (ID printf) ( (LITERAL "result : %d \n") , (ID a) ) ;) 
-})
 
