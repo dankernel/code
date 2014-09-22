86c28e420a1866a42ee1ba8e366858e7900e3c70
dankernel <google@dankook.ac.kr>
2014-09-23 01:10:01 +0900

    auto-20140923-011001

--- antlr/src/antlr/Argument.java
+++ antlr/src/antlr/Argument.java
@@ -2,9 +2,30 @@ package antlr;
 
 public class Argument extends v_argument {
 	
-	Val_type type;
-	String name;
-	Argument arg;
+	private Val_type type;
+	private String name;
+	private Argument arg;
+	
+	/* e.g., 
+	 * int a 	=> Int	: a	:
+	 * 10		=> Int	:	: 10
+	 * a		=> 		: a	:
+	 */
+	
+	public Argument (Val_type t, String n, Argument a)
+	{
+		this.type = t;
+		this.name = n;
+		this.arg = a;
+	}
+	
+	public Val_type get_type() {
+		try {
+			return this.type;
+		} catch (Exception e) {
+			return null;
+		}
+	}
 
 }
 
